f19b94d0573c758ecf95c8305175b1368436b158
Andreas Stührk <andy@hammerhartes.de>
2015-03-09 18:08:13 +0100

    Implement actual hipchat notifications.

--- src/cestranosys.ml
+++ src/cestranosys.ml
@@ -15,18 +15,42 @@ type event_obj = {
 } [@@deriving of_yojson, show]
 
 
-type notification_obj = {
+type notification = {
     message: string;
+    (* XXX a variant would be nice here, but unfortunately, ppx_deriving_json
+       represents variants as arrays *)
     message_format: string;
 } [@@deriving to_yojson]
 
 
 let docker_port = 2375
 
-let notify_room token event =
-  (* XXX hipchat notification would be here *)
-  print_endline (show_event_obj event);
-  flush stdout
+let notify_url room =
+  Uri.make ~scheme:"https" ~host:"hipchat.com"
+	   ~path:("/v2/room/" ^ room ^ "/notification") ()
+
+let pp_event event =
+  Printf.sprintf "Container %s (%s): %s"
+		 (String.sub event.id 0 12) event.from event.status
+
+let pp_uri_and_event uri event =
+  let host = match (Uri.host uri) with
+    | Some host -> host
+    | None -> "(unknown)"
+  in Printf.sprintf "%s: %s" host (pp_event event)
+
+let notify_room token room uri event =
+  let json = notification_to_yojson { message = pp_uri_and_event uri event;
+				      message_format = "text"; } in
+  let body = Cohttp_lwt_body.of_string (Yojson.Safe.to_string json)
+  and headers = Cohttp.Header.of_list [("Authorization", "Bearer " ^ token);
+				       ("Content-Type", "application/json")]
+  and uri = notify_url room in
+  let%lwt (response, _) = Cohttp_lwt_unix.Client.post ~body ~headers uri in
+  match response |> Response.status |> Code.code_of_status with
+  | 204 -> Lwt_io.printl ("Sent notification for " ^ (show_event_obj event))
+  | code -> Lwt_io.printlf "Unexpected status %d when sending %s"
+			   code (show_event_obj event)
 
 let handle_event notify data =
   let json = Yojson.Safe.from_string data in
@@ -34,16 +58,16 @@ let handle_event notify data =
   | `Ok event -> notify event
   | `Error error ->
      (* XXX log warning or something *)
-     ()
+     Lwt.return ()
 
-let run docker_uris =
+let run docker_uris room =
   let room_token =
     try Sys.getenv "HIPCHAT_ROOM_TOKEN" with
     Not_found ->
       print_endline "HIPCHAT_ROOM_TOKEN env var not set!";
       exit 1
   in
-  let notify = notify_room room_token
+  let notify = notify_room room_token room (List.hd docker_uris)
     and events_uri = Uri.with_path (List.hd docker_uris) "/events"
   in
   Lwt_main.run (
@@ -54,7 +78,7 @@ let run docker_uris =
 	failwith(Format.sprintf "Unexpected HTTP status: %d" code);
       let stream = (Cohttp_lwt_body.to_stream body) in
       (* XXX We assume that every iteration is a complete event *)
-      Lwt_stream.iter (handle_event notify) stream
+      Lwt_stream.iter_s (handle_event notify) stream
   )
 
 
@@ -73,11 +97,14 @@ let uri =
   ),
   fun ppf u -> Format.fprintf ppf "%s" (Uri.to_string u)
 
+let room =
+  Cmdliner.Arg.(required & pos 0 (some string) None & info [] ~docv:"ROOM")
+
 let docker_uris =
-  Cmdliner.Arg.(non_empty & (pos_all uri) [] & info [] ~docv:"DOCKER_URI")
+  Cmdliner.Arg.(non_empty & (pos_right 0 uri) [] & info [] ~docv:"DOCKER_URI")
 
 let run_t =
-  Cmdliner.Term.(pure run $ docker_uris)
+  Cmdliner.Term.(pure run $ docker_uris $ room)
 
 let () =
   match Cmdliner.Term.eval (run_t, info) with
