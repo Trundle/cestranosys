cdd8b9f53305f5cd3c40893c4d6f5ef3b392a593
Andreas Stührk <andy@hammerhartes.de>
2015-03-09 18:11:53 +0100

    Pass docker URI as parameter; prepare for hipchat notifications.

--- src/cestranosys.ml
+++ src/cestranosys.ml
@@ -12,22 +12,23 @@ type event_obj = {
     id: string;
     from: string;
     time: int;
-} [@@deriving of_yojson]
+} [@@deriving of_yojson, show]
+
+
+type notification_obj = {
+    message: string;
+    message_format: string;
+} [@@deriving to_yojson]
 
 
 let docker_port = 2375
 
-let events_uri host =
-  Uri.make ~scheme:"http" ~host:host ~port:docker_port ~path:"/events" ()
-
-let info =
-  Cmdliner.Term.info "cestranosys"
-
-let notify event =
-  Printf.printf "%s: %s\n" event.from event.status;
+let notify_room token event =
+  (* XXX hipchat notification would be here *)
+  print_endline (show_event_obj event);
   flush stdout
 
-let handle_event data =
+let handle_event notify data =
   let json = Yojson.Safe.from_string data in
   match (event_obj_of_yojson json) with
   | `Ok event -> notify event
@@ -35,22 +36,48 @@ let handle_event data =
      (* XXX log warning or something *)
      ()
 
-let run () =
-  print_endline "Beginning to look out for cetaceans...";
-
+let run docker_uris =
+  let room_token =
+    try Sys.getenv "HIPCHAT_ROOM_TOKEN" with
+    Not_found ->
+      print_endline "HIPCHAT_ROOM_TOKEN env var not set!";
+      exit 1
+  in
+  let notify = notify_room room_token
+    and events_uri = Uri.with_path (List.hd docker_uris) "/events"
+  in
   Lwt_main.run (
-      let%lwt (resp, body) = Client.get (events_uri "localhost") in
+      let%lwt (resp, body) = Client.get events_uri in
       let status = Response.status resp in
       let code = (Code.code_of_status status) in
       if (code != 200) then
 	failwith(Format.sprintf "Unexpected HTTP status: %d" code);
       let stream = (Cohttp_lwt_body.to_stream body) in
       (* XXX We assume that every iteration is a complete event *)
-      Lwt_stream.iter handle_event stream
+      Lwt_stream.iter (handle_event notify) stream
   )
 
+
+(* CLI follows *)
+
+let info =
+  Cmdliner.Term.info "cestranosys"
+
+(* Converts into a Uri.t *)
+let uri =
+  (fun s ->
+   let uri = Uri.of_string s in
+   match Uri.host uri with
+   | Some _ -> `Ok uri
+   | None -> `Error "Please use an URI with scheme set"
+  ),
+  fun ppf u -> Format.fprintf ppf "%s" (Uri.to_string u)
+
+let docker_uris =
+  Cmdliner.Arg.(non_empty & (pos_all uri) [] & info [] ~docv:"DOCKER_URI")
+
 let run_t =
-  Cmdliner.Term.(pure run $ pure ())
+  Cmdliner.Term.(pure run $ docker_uris)
 
 let () =
   match Cmdliner.Term.eval (run_t, info) with
